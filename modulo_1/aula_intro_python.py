fa9e5eac79b9a47361574ded93ccf267b390750f
leticiasampaio-ds <leticiasampaio.ds@gmail.com>
2024-10-31 20:36:38 -0300

    atualizei a aula 1-1

--- modulo_1/aula_intro_python.py
+++ modulo_1/aula_intro_python.py
@@ -1,1 +1,15 @@
-print("ola, mundo!")
+km_corridos = 10.5 #float
+km_corridos_concorrente = 12 # int
+nome_da_corrida = "Corrida IWTraining" # str
+duracao_da_corrida = [3.5, 3] # list
+ganhei_a_corrida = False # bool
+paradas = {"praça": 5, "em frente a igreja": 3.5} # Chave:Valor (dict)
+
+# Condicionais (If, Elif, Else)
+for item in range(1,10):
+    if item == 5:
+        print(f'meu numero é {item}')
+    elif item / 2 == 4:
+        print(f'resultado da divsao por 2 é 4: item - {item}')
+    else:
+        print(f'é o que sobrou {item}')
