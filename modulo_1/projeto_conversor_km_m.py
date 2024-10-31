b4a9afed36b16d9a2c27c4cc371a3ed5a13d53f9
leticiasampaio-ds <leticiasampaio.ds@gmail.com>
2024-10-31 20:36:56 -0300

    criei arquivo de exercicio

---
+++ modulo_1/projeto_conversor_km_m.py
@@ -0,0 +1,5 @@
+# Criar um programa que converta um valor de KM para M. (N/1000)
+## Regras:
+## 1. O usuário vai inserir um número.
+## 2. Criar uma função que execute a conversão.
+## 3. Apresentar para o usuário o valor convertido.
