8debd78b75663311b95e34d25c062bbde469dc78
leticiasampaio-ds <leticiasampaio.ds@gmail.com>
2024-10-31 21:15:35 -0300

    finalizei o conversor

--- modulo_1/projeto_conversor_km_m.py
+++ modulo_1/projeto_conversor_km_m.py
@@ -1,5 +1,19 @@
-# Criar um programa que converta um valor de KM para M. (N/1000)
+# Criar um programa que converta um valor de KM para M. (N * 1000)
 ## Regras:
 ## 1. O usuário vai inserir um número.
 ## 2. Criar uma função que execute a conversão.
 ## 3. Apresentar para o usuário o valor convertido.
+
+# Regra 1
+input_em_km = input("Insira o valor em KM: ")
+input_em_km_int = int(input_em_km)
+
+# Regra 2
+def conversao_km_m(n_km):
+    resultado_conversao = n_km * 1000
+    return resultado_conversao
+
+resultado = conversao_km_m(input_em_km_int)
+
+# Regra 3
+print(f"Esse é seu número em metros: {resultado}")
